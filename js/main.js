8adf4eec198697458131c8bec01857c4cd332a8d
JoatanMoura <jothah@gmail.com>
2022-01-31 14:07:18 -0300

    primeira aula de JS

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var nome = "Joatan Moura";
+var idade = 47;
+var idade2 = 8;
+var frase = "Japão é o melhor time do mundo."
+//alert(nome + " tem " + idade + " anos.");
+//alert(idade+idade2);
+console.log(nome);
+console.log(idade2);
+console.log(frase.replace("Japão","Brasil"));
+alert(frase.toLowerCase());
